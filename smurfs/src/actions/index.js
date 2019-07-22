e0ab65a6110d5c60af03ab9e87678b4afb35bf2a
Soraia <soraia@Soraias-iMac.local>
2019-07-21 20:30:44 -0400

    updated action/action creator in src/actions/index.js

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,3 +1,26 @@
+import axios from "axios";
+
+export const FETCHING_SMURFS = "FETCHING_SMURFS";
+export const FETCHING_SMURFS_SUCCESS = "FETCHING_SMURFS_SUCCESS";
+export const FETCHING_SMURFS_FAILURE = "FETCHING_SMURFS_FAILURE";
+
+export const getSmurfs = () => dispatch => {
+    dispatch({ type: FETCHING_SMURFS });
+    axios
+        .get("http://localhost:3333/api/smurfs")
+        .then(({ data }) => {
+          dispatch({
+            type: FETCHING_SMURFS_SUCCESS,
+            payload: data.results
+          });
+        })
+        .catch(err => {
+          dispatch({
+            type: FETCHING_SMURFS_FAILURE,
+            payload: err
+          });
+        });
+};
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
